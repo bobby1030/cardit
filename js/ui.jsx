5b9667835c384c2df837e8600945787d400ebc5d
BobbyHo <angela.bobby@gmail.com>
2016-05-07 14:55:17 +0800

    Finish Basic Fucntion

--- js/ui.jsx
+++ js/ui.jsx
@@ -1,26 +1,63 @@
 var Card = React.createClass({
 
     render: function() {
+
+    	var content = this.props.json.map(function(arg){
+
+		return(
+    		<div className="ui fluid card">
+    		<a className="image" href={arg.data.preview.images[0].source.url}>
+          			<img src={arg.data.preview.images[0].source.url} />
+        	</a>
+        	<div className="content">
+          		<a className="header" href={arg.data.url}>{arg.data.title}</a>
+          		<div className="meta">
+            		<span className="date">GIVE ME A DATE</span>
+          		</div>
+        	</div>
+        	</div>
+        	)
+    	})
+
         return (
-            <div className="ui fluid card">
-        		<a className="image" href={this.props.image}>
-          			<img src={this.props.image} />
-        		</a>
-        		<div className="content">
-          			<a className="header" href={this.props.post}>I'm A Title</a>
-          			<div className="meta">
-            			<span className="date">Give Me A Date</span>
-          			</div>
-        		</div>
-        		<a href={this.props.image} download>
-          			<div className="ui bottom attached button">
-            			<i className="download icon" /> Download
-          			</div>
-        		</a>
-      		</div>
+        	<div>
+        	{content}
+        	</div>
         );
     }
 
 });
 
-ReactDOM.render(<Card image="http://fakeimg.pl/1280x720" post="http://fakeimg.pl/1280x720"/>, document.getElementById('content'));
+var DATA
+var DATA_limit = 5
+
+function fetch_data() {
+
+	var subreddit = location.hash.replace(/\#/,'')
+	if (subreddit) {
+		$.getJSON('http://www.reddit.com/r/' + subreddit + '/new.json?' + 'limit=' + DATA_limit, function(data){
+			DATA = data
+			render()
+		}) // Fetch subreddit data
+	}else{
+		$.getJSON('http://www.reddit.com/new.json?' + 'limit=' + DATA_limit, function(data){
+			DATA = data
+			render()
+		})	// If no subreddit given, fetch reddit homepage.	
+	}
+}
+
+// function parse_data() {
+// 	DATA_rawjson.data.children.forEach(function(element){
+// 		DATA.image.push(element.data.preview.images[0].source.url)
+// 	})
+// 	console.log(DATA.image)
+// 	render()
+//	
+// }
+
+function render() {
+	ReactDOM.render(<Card json={DATA.data.children} />, document.getElementById('content'));
+}
+
+fetch_data()
