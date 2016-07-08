3d95d0ff31b04936b39aecffbfce6e265da5140e
BobbyHo <angela.bobby@gmail.com>
2016-07-08 13:37:52 +0800

    Finish Headroom.js implement

--- js/Menu.jsx
+++ js/Menu.jsx
@@ -1,6 +1,6 @@
 var React = require('react')
 var $ = require('jquery')
-require('./lib/jquery.transit.min.js')
+var Headroom = require('react-headroom').default
 
 var Menu = React.createClass({
 
@@ -30,72 +30,31 @@ var Menu = React.createClass({
 
     render: function() {
         return (
-            <nav>
-              <div className="ui three item blue inverted tiny stackable fixed menu">
-                <a href="/" className="header item">Cardit</a>
-                <div className="item">
-                  <form onSubmit={ this.handleSubmit }>
-                    <div className="ui left icon input">
-                      <input name="subreddit" placeholder="Goto" type="text" value={ this.state.subreddit } onChange={ this.handleSubredditChange } />
-                      <i className="reddit square inverted circular icon"></i>
-                    </div>
-                  </form>
-                  <form onSubmit={ this.handleSubmit }>
-                    <div className="ui left icon input">
-                      <input name="limit" placeholder="Cards" type="text" value={ this.state.limit } onChange={ this.handleLimitChange } />
-                      <i className="filter inverted circular icon"></i>
-                    </div>
-                  </form>
+            <nav id="menu">
+              <Headroom>
+                <div className="ui three item blue inverted tiny stackable menu">
+                  <a href="/" className="header item">Cardit</a>
+                  <div className="item">
+                    <form onSubmit={ this.handleSubmit }>
+                      <div className="ui left icon input">
+                        <input name="subreddit" placeholder="Goto" type="text" value={ this.state.subreddit } onChange={ this.handleSubredditChange } />
+                        <i className="reddit square inverted circular icon"></i>
+                      </div>
+                    </form>
+                    <form onSubmit={ this.handleSubmit }>
+                      <div className="ui left icon input">
+                        <input name="limit" placeholder="Cards" type="text" value={ this.state.limit } onChange={ this.handleLimitChange } />
+                        <i className="filter inverted circular icon"></i>
+                      </div>
+                    </form>
+                  </div>
+                  <a href="https://github.com/bobby1030/cardit" className="ui item mobile-hide">
+                    <i className="github icon"></i> GitHub Repo
+                  </a>
                 </div>
-                <a href="https://github.com/bobby1030/cardit" className="ui item mobile-hide">
-                  <i className="github icon"></i> GitHub Repo
-                </a>
-              </div>
+              </Headroom>
             </nav>
         )
-    },
-
-    componentDidMount: function() {
-        var didScroll;
-        var lastScrollTop = 0;
-        var delta = 250;
-        var navbarHeight = $('nav').outerHeight();
-
-        $(window).scroll(function(event) {
-            didScroll = true;
-        });
-
-        setInterval(function() {
-            if (didScroll) {
-                hasScrolled();
-                didScroll = false;
-            }
-        }, 100);
-
-        function hasScrolled() {
-            var st = $(this).scrollTop();
-
-            // Make sure they scroll more than delta
-            if (Math.abs(lastScrollTop - st) <= delta)
-                return;
-
-            // If they scrolled down and are past the navbar, add class .nav-up.
-            // This is necessary so you never see what is "behind" the navbar.
-            if (st > lastScrollTop && st > navbarHeight) {
-                // Scroll Down
-                $('nav > div').transition({
-                    y: '-10em'
-                })
-            } else if (st + $(window).height() < $(document).height()) {
-                // Scroll Up
-                $('nav > div').transition({
-                    y: 0
-                });
-            }
-
-
-            lastScrollTop = st;
-        }
     }
 })
 
